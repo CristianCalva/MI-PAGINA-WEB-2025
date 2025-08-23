98f392cb27f180d4c2f6df2314cd1563ddb11862
Paul <cp.calvaa@uea.edu.ec>
2025-08-23 17:01:01 -0500

    SEMANA 10

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-#Aplicación en Flask
-from turtle import title
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def indexfl():
-    #return '¡Hola! Bienvenido a mi aplicación Flask.'
-    return render_template('index.html', title='Inicio') 
+def index():
+    return render_template('index.html', title='Inicio')
 
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
@@ -15,13 +12,11 @@ def usuario(nombre):
 
 @app.route('/about')
 def about():
-    #return "Este es una aplicación de ejemplo Flask"
-    return render_template('about.html', title='Aserca de') 
-
+    return render_template('about.html', title='Acerca de')
 
 @app.route('/contacto')
 def contacto():
-    return "Contactate con nosotros en paulcalva95"
+    return "Contáctate con nosotros en paulcalva95"
 
 if __name__ == '__main__':
     app.run(debug=True)
