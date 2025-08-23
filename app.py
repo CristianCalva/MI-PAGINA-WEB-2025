73dbbea4e53cc13471c4b1dea98124d17820ab15
Paul <cp.calvaa@uea.edu.ec>
2025-08-23 13:34:50 -0500

    semana 10

--- app.py
+++ app.py
@@ -4,21 +4,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return '¡Hola! Bienvenido a mi aplicación Flask.'
 
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
     return f'Bienvenido, {nombre}!'
 
+@app.route('/acerca',strict_slashes=False)
+def acerca():
+    return "Este es una aplicación de ejemplo Flask"
+
 @app.route('/contacto')
 def contacto():
-    return "Contacta con nosotros en contacto@midominio.com"
-
-@app.route('/servicios')
-def servicios():
-    return "Aquí puedes ver todos los servicios que ofrece nuestra aplicación."
-
+    return "Contactate con nosotros en paulcalva95"
 
 if __name__ == '__main__':
     app.run(debug=True)
